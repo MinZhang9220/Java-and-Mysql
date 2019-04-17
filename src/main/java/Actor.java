8fca7f030556c7a57940166d0f212bfc3856121a
Billythekidzz <gon12@uclive.ac.nz>
2019-04-17 14:49:25 +1200

    untracked all target files

--- src/main/java/Actor.java
+++ src/main/java/Actor.java
@@ -1,6 +1,31 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class Actor {
 
-    public String firstName;
-    public String lastName;
-    public double levelOfTrust;
+    private int actorid;
+    private String firstName;
+    private String lastName;
+    private double levelOfTrust;
+
+    /**
+     * Constructs an Actor with a given actor id, first name, last name and level of trust.
+     * @param actorid the actor's id
+     * @param firstName the actor's first name
+     * @param lastName the actor's last name
+     * @param levelOfTrust the actor's level of trust
+     */
+    public Actor(int actorid, String firstName, String lastName, double levelOfTrust){
+        this.actorid = actorid;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.levelOfTrust = levelOfTrust;
+    }
+
+    /**
+     * Inserts the actor and its attributes into the database where it will be stored persistently.
+     */
+    public void insertActorIntoDatabase(Connection connection, Actor actor){
+        //PreparedStatement statement = connection.
+    }
 }
